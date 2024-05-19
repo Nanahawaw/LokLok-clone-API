e1315bf64e8b6d899505e80f65564f334291c434
Nanahawaw <folarinyetunde@gmail.com>
2024-05-19 15:32:58 +0100

    modifies userschema to store otp secret for verification

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,6 +5,7 @@ import { generateOtp } from "../utils/otpGenerator.js";
 
 export const registerUser = async (req, res) => {
     try {
+        const { otp, secret } = generateOtp()
         const newUser = new User({
             email: req.body.email,
             password: await bcrypt.hash(req.body.password, 10),
@@ -12,7 +13,6 @@ export const registerUser = async (req, res) => {
         })
         const user = await newUser.save();
         //verify email by sending otp
-        const otp = generateOtp();
         const emailOptions = {
             to: user.email,
             subject: `To verify your account , please enter the following verification code on LOKLOK: ${otp}. The veriication code expires in 1 hour. if you do not request the code please ignore this message
