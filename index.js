a2ddba75deb3390dd99ab977350cc27666f0808a
Nanahawaw <115956826+Nanahawaw@users.noreply.github.com>
2024-05-21 20:33:49 +0000

    Deploying to gh-pages from @ Nanahawaw/LokLok-clone-API@bfc93aebeeb69cbc1b7eeb70bc902f8878b47c16 🚀

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import authRouter from './routes/auth.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import swaggerUi from 'swagger-ui-express';
-import fs from 'fs';
-import path from 'path';
+
 
 dotenv.config();
 
@@ -18,14 +17,15 @@ mongoose.connect(process.env.MONGO
 
 const app = express();
 
-// Check if swagger-output.json exists before importing it
-const swaggerOutputPath = path.resolve('swagger-output.json');
-if (fs.existsSync(swaggerOutputPath)) {
-    const swaggerFile = JSON.parse(fs.readFileSync(swaggerOutputPath, 'utf-8'));
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-} else {
-    console.error('Swagger output file not found. Please run the swagger.js script first.');
-}
+/// Serve Swagger UI from GitHub Pages
+const swaggerUrl = 'https://Nanahawaw.github.io/LokLok-clone-API/swagger-output.json';
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup({
+        swaggerUrl,
+    })
+);
 app.use(express.json());
 app.use(cors())
 app.use(cookieParser());
