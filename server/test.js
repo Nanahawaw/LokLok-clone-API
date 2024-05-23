cb5e55f3fd8d58ac3bea98c6bd3fcc258c563364
Nanahawaw <folarinyetunde@gmail.com>
2024-05-23 15:35:55 +0100

    added base path

--- server/test.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const authFilePath = path.join(__dirname, 'routes', 'auth.js');
-
-console.log('Path to auth.js file:', authFilePath);
