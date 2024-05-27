daf8b86fef69636fc7acfc63a865ddbd98bed105
Nanahawaw <folarinyetunde@gmail.com>
2024-05-27 09:20:57 +0100

    get all users by admin

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,4 +1,11 @@
 import User from '../models/userModel.js';
 
 
-export const getUsers = async (req, res) => { }
+export const getUsers = async (req, res) => {
+
+    const users = await User.find({}).limit(10);
+    res.status(200).json(users);
+    if (users.length < 0) {
+        res.status(404).json({ message: 'No users found' });
+    }
+}
