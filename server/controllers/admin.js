0c75d5f818ddfb9c1997c6d07b226904e7378bf6
Nanahawaw <folarinyetunde@gmail.com>
2024-05-27 09:31:16 +0100

    get user by id

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -9,3 +9,18 @@ export const getUsers = async (req, res) => {
         res.status(404).json({ message: 'No users found' });
     }
 }
+
+export const getUserById = async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const user = await User.findById(id);
+        if (!user) {
+            res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+}
